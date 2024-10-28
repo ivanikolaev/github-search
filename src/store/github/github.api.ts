2ea2e38888fd609ff1d020efb981ad21bc6222fd
ivanikolaev <goturpokla@gmail.com>
2024-10-29 02:58:06 +0300

    added users search

--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -4,6 +4,7 @@ import { IUser, ServerResponse, SearchUsersParams } from '../../models/models'
 export const githubApi = createApi({
     reducerPath: 'github/api/v1',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com/' }),
+    refetchOnFocus: true,
     endpoints: (build) => ({
         searchUsers: build.query<IUser[], SearchUsersParams>({
             query: (search: SearchUsersParams ) => ({
